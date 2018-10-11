129b89baf0adb95ef812ff142dc9b89ed8344185
chai2010 <chaishushan@gmail.com>
2018-10-11 14:22:27 +0800

    zz

---
+++ Makefile
@@ -0,0 +1,6 @@
+default:
+
+mod:
+	HTTPS_PROXY=socks5://127.0.0.1:2080 go test
+
+clean:
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	_ "github.com/golang/protobuf/proto"
+	_ "google.golang.org/grpc"
+)
+
+func main() {
+	println("hi etcd")
+}
