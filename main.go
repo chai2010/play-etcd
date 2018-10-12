7f0b536f2f07c26ded8fb872c5567215361436a3
chai2010 <chaishushan@gmail.com>
2018-10-12 17:51:42 +0800

    add pflag

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/golang/protobuf/proto"
 	_ "github.com/grpc-ecosystem/grpc-gateway/runtime"
 	_ "github.com/pkg/errors"
+	_ "github.com/spf13/pflag"
 	_ "github.com/yuin/gopher-lua"
 	_ "golang.org/x/crypto/bcrypt"
 	_ "golang.org/x/image/bmp"
